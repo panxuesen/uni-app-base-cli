df8d34b8dd291fb35a295c424099a0f7d24d872f
panxuesen <13552198363@163.com>
2020-06-02 14:49:00 +0800

    获取系统信息

--- src/common/index.js
+++ src/common/index.js
@@ -7,6 +7,7 @@
 export function COMMON (Vue) {
     Vue.config.productionTip = false
     Vue.prototype.$getShareInfo = getShareInfo
+    getSystemInfo(Vue)
 }
 
 /**
@@ -20,4 +21,23 @@ export function getShareInfo (obj = {}) {
         path: `/pages/index/index`
     }
     return Object.assign(shareObj, obj)
+}
+
+export function getSystemInfo (Vue) {
+    let systemInfo = uni.getSystemInfoSync()
+    let menuInfo = uni.getMenuButtonBoundingClientRect()
+    // 获取手机型号
+    if (systemInfo.model.search('iPhone X') != -1 && Vue) {
+        Vue.prototype.isIPhoneX = true
+    }
+    // 获取导航栏宽高 自定义导航栏使用  系统字体大小 systemInfo.fontSizeSetting
+    let statusBar = systemInfo.statusBarHeight
+    let barHeight = menuInfo.height + (menuInfo.top - statusBar) * 2 + statusBar
+    let barInfo = {
+        barHeight: barHeight,
+        titleHeight: menuInfo.height,
+        titleTop: menuInfo.top
+    }
+    Vue.prototype.$barInfo = barInfo
+    return systemInfo
 }
